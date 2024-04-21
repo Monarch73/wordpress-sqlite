b7bfdaa964f9e5173cf7dc10578fc4f42bee462e
Niels H <Nielsh@monarch.de>
2024-04-21 21:42:15 +0200

    taken install script from soulteary

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 sed -i '/exec "$@"/i nginx' /usr/local/bin/docker-entrypoint.sh
 
 echo "installing curl and unzip"
-apk add --update curl unzip
+apk add --update curl
 apk add nginx
 
 echo "clear cache"
@@ -14,13 +14,23 @@ rm -Rf /var/cache/apk/*
 
 
 echo "download Sqlite integration plugin"
-curl -o /tmp/wpplugin.zip https://downloads.wordpress.org/plugin/sqlite-database-integration.zip
+curl -L -o /tmp/sqlite-database-integration.tar.gz "https://github.com/WordPress/sqlite-database-integration/archive/refs/tags/v2.1.9.tar.gz"
 
-echo "unzip Sqlite integration plugin"
-unzip /tmp/wpplugin.zip -d /usr/src/wordpress/wp-content/plugins/
+echo "untar Sqlite integration plugin"
+tar zxvf sqlite-database-integration.tar.gz -c /tmp/
 
-echo "removing zip file"
-rm /tmp/wpplugin.zip
+#install
+mkdir -p /usr/src/wordpress/wp-content/mu-plugins/sqlite-database-integration 
+cp -r /tmp/sqlite-database-integration/* /usr/src/wordpress/wp-content/mu-plugins/sqlite-database-integration/
+mv "/usr/src/wordpress/wp-content/mu-plugins/sqlite-database-integration/db.copy" "/usr/src/wordpress/wp-content/db.php"
+sed -i 's#{SQLITE_IMPLEMENTATION_FOLDER_PATH}#/var/www/html/wp-content/mu-plugins/sqlite-database-integration#' "/usr/src/wordpress/wp-content/db.php"
+sed -i 's#{SQLITE_PLUGIN}#sqlite-database-integration/load.php#' "/usr/src/wordpress/wp-content/db.php"
+mkdir "/usr/src/wordpress/wp-content/database"
+touch "/usr/src/wordpress/wp-content/database/.ht.sqlite"
+chmod 640 "/usr/src/wordpress/wp-content/database/.ht.sqlite"
+
+echo "removing tar file"
+rm /tmp/sqlite-database-integration.tar.gz
 
 echo "setting up wp-config.php"
 #cp /usr/src/wordpress/wp-content/plugins/sqlite-database-integration/wp-includes/sqlite/db.php /usr/src/wordpress/wp-content
