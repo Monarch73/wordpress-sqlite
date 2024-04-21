dd89beba39dfc6b82a5baf07a11d36b4549b2927
Niels H <Nielsh@monarch.de>
2024-04-21 17:16:54 +0200

    fixing stuff

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #sed -i 's/pm = dynamic/pm = ondemand/g' /usr/local/etc/php-fpm.d/www.conf
 
 # insert line "exec nginx" before a line containing 'exec "$@"' in /usr/local/bin/docker-entrypoint.sh using sed
-sed -i '/exec "$@"/i exec nginx' /usr/local/bin/docker-entrypoint.sh
+sed -i '/exec "$@"/i nginx' /usr/local/bin/docker-entrypoint.sh
 
 echo "installing curl and unzip"
 apk add --update curl unzip
@@ -23,11 +23,11 @@ echo "removing zip file"
 rm /tmp/wpplugin.zip
 
 echo "setting up wp-config.php"
-cp /usr/src/wordpress/wp-content/plugins/sqlite-database-integration/wp-includes/sqlite/db.php /usr/src/wordpress/wp-content
-cp /tmp/default.conf /etc/nginx/http.d/default.conf
+#cp /usr/src/wordpress/wp-content/plugins/sqlite-database-integration/wp-includes/sqlite/db.php /usr/src/wordpress/wp-content
+cp /tmp/config/default.conf /etc/nginx/http.d/default.conf
 
-echo "copying wp-config.php"
-cp /tmp/config/wp-config.php /var/www/wp-config.php
+#echo "copying wp-config.php"
+#cp /tmp/config/wp-config.php /usr/src/wordpress/wp-config.php
 
-echo "chown wp-config.php"
-chown www-data:www-data /var/www/wp-config.php
+#echo "chown wp-config.php"
+#chown www-data:www-data /var/www/wp-config.php
